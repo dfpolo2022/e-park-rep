a991da0594754114a5600683cf0382cc376e24fc
Ivan Perez <ivanestebanperezdlo@hotmail.com>
2022-11-10 15:48:36 -0500

    feat: Firebase conection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bottom_navigation_with_nested_routing_tutorial/routes/router.gr.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:loggy/loggy.dart';
+import 'settings/configuration.dart';
 
-void main() => runApp(AppWidget());
+// void main() => runApp(AppWidget());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(
+      showColors: true,
+    ),
+  );
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: Configuration.apiKey,
+      authDomain: Configuration.authDomain,
+      databaseURL: Configuration.databaseURL,
+      projectId: Configuration.projectId,
+      messagingSenderId: Configuration.messagingSenderId,
+      appId: Configuration.appId,
+  ));
+  runApp(AppWidget());
+}
+
 
 class AppWidget extends StatelessWidget {
   AppWidget({Key? key}) : super(key: key);
--- lib/settings/configuration.dart
+++ lib/settings/configuration.dart
@@ -1,11 +1,10 @@
 class Configuration {
-  static const apiKey = "AIzaSyA17KpG9UxsrHrWrWrfM2qQcDrMSNWcDa8";
-  static const authDomain = "fir-taller-ivanperez.firebaseapp.com";
-  static const databaseURL =
-      "https://fir-taller-ivanperez-default-rtdb.firebaseio.com";
-  static const projectId = "fir-taller-ivanperez";
-  static const storageBucket = "fir-taller-ivanperez.appspot.com";
-  static const appId = "1:734096871113:android:b05faad97758187283c718";
-  static const messagingSenderId = "734096871113";
-  static const measurementId = "G-96TTLGKKTS";
+  static const apiKey = "AIzaSyD71x032bVPuYmEEQ1eWYkWjeQ4cZuqieo";
+  static const authDomain = "e-park-74807.firebaseapp.com";
+  static const databaseURL = "https://e-park-74807-default-rtdb.firebaseio.com";
+  static const projectId = "e-park-74807";
+  static const storageBucket = "e-park-74807.appspot.com";
+  static const messagingSenderId = "257027785799";
+  static const appId = "1:257027785799:web:089e59944ee9bde58a5d62";
+  static const measurementId = "G-20GR4GGEQV";
 }
