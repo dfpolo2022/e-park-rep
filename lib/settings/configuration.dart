16fed4d7947efbcac0534863add08d63949903b8
Ivan Perez <ivanestebanperezdlo@hotmail.com>
2022-11-10 12:17:18 -0500

    feat: auth controller

---
+++ lib/settings/configuration.dart
@@ -0,0 +1,11 @@
+class Configuration {
+  static const apiKey = "AIzaSyA17KpG9UxsrHrWrWrfM2qQcDrMSNWcDa8";
+  static const authDomain = "fir-taller-ivanperez.firebaseapp.com";
+  static const databaseURL =
+      "https://fir-taller-ivanperez-default-rtdb.firebaseio.com";
+  static const projectId = "fir-taller-ivanperez";
+  static const storageBucket = "fir-taller-ivanperez.appspot.com";
+  static const appId = "1:734096871113:android:b05faad97758187283c718";
+  static const messagingSenderId = "734096871113";
+  static const measurementId = "G-96TTLGKKTS";
+}
